f52b2e58f808b5fd1eb7630cc912784afe4f663c
aarohi1313 <badheanushka950@gmail.com>
2026-07-19 20:54:51 +0530

    Add files via upload

---
+++ average of 5 number.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10, b = 20, c = 30, d = 40, e = 50;
+    float average = (a + b + c + d + e) / 5.0;
+
+    printf("Average = %.2f", average);
+
+    return 0;
+}
