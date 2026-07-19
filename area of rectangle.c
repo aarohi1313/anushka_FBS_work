db6e8a56013a79db815fd109dc206834837779fe
aarohi1313 <badheanushka950@gmail.com>
2026-07-19 20:53:27 +0530

    Add files via upload

---
+++ area of rectangle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    float base = 10;
+    float height = 5;
+
+    float area = 0.5 * base * height;
+
+    printf("Base = %.2f\n", base);
+    printf("Height = %.2f\n", height);
+    printf("Area of Triangle = %.2f\n", area);
+
+    return 0;
+}
