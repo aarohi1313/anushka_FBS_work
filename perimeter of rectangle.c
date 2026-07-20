656053de4418f9e5d1d09e001b89fdabf211c1a8
aarohi1313 <badheanushka950@gmail.com>
2026-07-20 10:21:08 +0530

    Add files via upload

---
+++ perimeter of rectangle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int length = 10;
+    int width = 5;
+
+    int perimeter = 2 * (length + width);
+
+    printf("Length = %d\n", length);
+    printf("Width = %d\n", width);
+    printf("Perimeter = %d\n", perimeter);
+
+    return 0;
+}
