24832918d36943d8c2581bb3c5305bcdea3d5fa7
aarohi1313 <badheanushka950@gmail.com>
2026-07-20 10:20:40 +0530

    Add files via upload

---
+++ find area of circle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    float radius = 7;
+    float area = 3.14 * radius * radius;
+
+    printf("Radius = %.2f\n", radius);
+    printf("Area of Circle = %.2f", area);
+
+    return 0;
+}
