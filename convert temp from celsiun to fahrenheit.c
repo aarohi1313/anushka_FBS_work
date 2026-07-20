6a3da9b7bd0e1c4701946fe752677fbb11215da5
aarohi1313 <badheanushka950@gmail.com>
2026-07-20 10:20:08 +0530

    Add files via upload

---
+++ convert temp from celsiun to fahrenheit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    float c = 25;
+    float f = (c * 9 / 5) + 32;
+
+    printf("Celsius = %.2f\n", c);
+    printf("Fahrenheit = %.2f", f);
+
+    return 0;
+}
