9dfea033449db0604d7fa0e43384e3e68cf73122
aarohi1313 <badheanushka950@gmail.com>
2026-07-20 10:21:32 +0530

    Add files via upload

---
+++ sqaure and cube.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int num = 5;
+    int square = num * num;
+    int cube = num * num * num;
+
+    printf("Number = %d\n", num);
+    printf("Square = %d\n", square);
+    printf("Cube = %d\n", cube);
+
+    return 0;
+}
