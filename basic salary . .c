7405c2fb5b647fc917416e366e6ba5ab2dae6510
aarohi1313 <badheanushka950@gmail.com>
2026-07-24 21:31:37 +0530

    Add files via upload

---
+++ basic salary . .c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    double basic, da, ta, hra, total;
+
+    printf("Enter the basic salary: ");
+    scanf("%lf", &basic);
+
+    if (basic <= 5000)
+    {
+        da = basic * 0.10;
+        ta = basic * 0.20;
+        hra = basic * 0.25;
+    }
+    else
+    {
+        da = basic * 0.15;
+        ta = basic * 0.25;
+        hra = basic * 0.30;
+    }
+
+    total = basic + da + ta + hra;
+
+    printf("Total salary = %.2lf", total);
+
+    return 0;
+}
---
+++ uppercase or lowercase.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+	char ch = 'A' ;
+	
+	  if (ch >= 'A' && ch <= 'Z')
+	 
+	      printf("uppercase");
+	      
+	    else 
+		     if(ch >= 'a' && ch <= 'z') 
+		     
+			 printf("lowercase");
+		
+		else 
+			 
+			 printf("not a alphabet");
+			 
+
+	
+}
