703c282faa113363449b8e718ff49dd5f24424a4
aarohi1313 <badheanushka950@gmail.com>
2026-07-24 21:30:56 +0530

    Add files via upload

---
+++ Leap or not leap year.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+	int year = 2024;
+
+		 if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+		
+		  printf("leap year");
+	else
+	
+	     printf("not a leap year");
+		 
+		 
+		 
+		  
+		   
+
+	
+	
+	
+	
+}
---
+++ eligible to vote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int age = 18;
+	
+	  if(age > 18)
+	  
+	  printf("eligible");
+	  
+	else 
+	    
+		printf("not eligible"); 
+	
+	
+	
+}
