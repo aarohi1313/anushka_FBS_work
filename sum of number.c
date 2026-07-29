882dc7eac80845b05e9e74234c49657fbde3a55b
Anushka <badheanushka950@gmail.com>
2026-07-29 21:26:29 +0530

    Add files via upload

---
+++ sum of number.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int start, end, sum = 0, i;
+
+    printf("Enter start and end: ");
+    scanf("%d%d", &start, &end);
+
+    for(i = start; i <= end; i++)
+    {
+        sum = sum + i;
+    }
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
